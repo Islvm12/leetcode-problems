655aa69365d13ce20008ce8177e8b56ea4b7cbb4
Islam Muhammed <islammu7ammed.official@gmail.com>
2026-02-04 15:53:36 +0200

    Time: 4 ms (90.08%), Space: 64.2 MB (54.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -4,5 +4,12 @@
  */
 var isPalindrome = function (x) {
     if (x < 0) return false;
-    return +(x).toString().split('').reverse().join("") === x
+    let original = x
+    let reversed = 0
+
+    while (original > 0) {
+        reversed = reversed * 10 + original % 10
+        original = Math.floor(original / 10)
+    }
+    return x === reversed
 };
