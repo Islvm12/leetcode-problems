32873308057a0010ebae1f2219397a38598cb5b2
Islam Muhammed <islammu7ammed.official@gmail.com>
2026-02-06 17:45:26 +0200

    Time: 75 ms (100%), Space: 72.3 MB (63.64%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.js
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.js
@@ -3,6 +3,9 @@
  * @param {number} k
  * @return {number}
  */
+
+// Elfekra inna 3mlna maxIndex w minIndex lma el-while loop bttksr el-maxIndex bycoon azyad mn el-max value b wa7d f bnbda2 nkarn el=min el-gdeed b el-maxIndex el-7aly elly howa kasr el-while loop w 34an nfs el-sabb mgma3na4 wa7d "maxI - minI" hna 34an e7na aslan azyad mn el=max el7a2e2e b wa7d
+
 var minRemoval = function(nums, k) {
   nums.sort((a, b) => a - b);
 
