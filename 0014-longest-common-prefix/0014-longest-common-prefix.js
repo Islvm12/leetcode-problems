57a50a276e5192a502de1a31bccaf57e681038f9
Islam Muhammed <islammu7ammed.official@gmail.com>
2026-02-13 11:34:29 +0200

    Time: 0 ms (100%), Space: 54.4 MB (57.16%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -5,22 +5,15 @@
 var longestCommonPrefix = function (strs) {
 
     let prefix = strs[0]
-    let prefixFound = 0
 
-    function loop(arr) {
-        for (let i = 0; i < strs.length; i++) {
-            arr[i].slice(0,prefix.length).includes(prefix) ? prefixFound += 1 : "";
-        }
-        if (prefixFound === strs.length) {
-            return prefix;
-        } else if (prefix.length === 1) {
-            return prefix="";
-        } else {
+    for (let i = 1; i < strs.length; i++) {
+
+        while (!strs[i].startsWith(prefix)) {
             prefix = prefix.slice(0, -1)
-            prefixFound=0
-            loop(strs);
+
+            if (prefix === "") return ""
         }
     }
-    loop(strs)
-    return prefix;
+
+    return prefix
 };
