2f2430d4c72402033546aae8ddd83eab94bbe316
Islam Muhammed <islammu7ammed.official@gmail.com>
2026-02-23 11:51:23 +0200

    Time: 1 ms (47.45%), Space: 54.1 MB (50.88%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,6 +3,18 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
+var strStr = function (haystack, needle) {
+    let pointer = needle.length;
+    let haySize = haystack.length;
+
+    if (pointer > haySize) return -1;
+
+    for (let i = 0; i < haySize - pointer + 1; i++) {
+        if (haystack.slice(i, i + pointer) === needle) {
+            return i;
+        } else if (i === haySize - pointer) {
+            return -1;
+        }
+
+    }
 };
