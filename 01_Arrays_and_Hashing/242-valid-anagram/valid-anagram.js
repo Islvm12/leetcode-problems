06f338cb0b442fab21878595af855021feb19b3d
Islam Muhammed <islammu7ammed.official@gmail.com>
2026-03-17 12:31:39 +0200

    Time: 6 ms (93.25%) | Memory: 55.7 MB (74.19%) - LeetSync

--- 01_Arrays_and_Hashing/242-valid-anagram/valid-anagram.js
+++ 01_Arrays_and_Hashing/242-valid-anagram/valid-anagram.js
@@ -5,14 +5,13 @@
  */
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false
-        let hash1 = {};
-        let hash2 = {};
-        for (let i=0; i < t.length; i++){
-            hash1[s[i]] ? hash1[s[i]] += 1 : hash1[s[i]] = 1
-            hash2[t[i]] ? hash2[t[i]] += 1 : hash2[t[i]] = 1
-        }
-        for (let key in hash1){
-            if (hash1[key] !== hash2[key]) return false
-        }
-        return true
+        let hash1 = {};
+        for (let i=0; i < t.length; i++){
+            hash1[s[i]] = (hash1[s[i]] || 0) + 1;
+            hash1[t[i]] = (hash1[t[i]] || 0) - 1;
+        }
+        for (let key in hash1){
+            if (hash1[key] !== 0) return false
+        }
+        return true
 };
