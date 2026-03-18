b82852d5755af3b65cfe04e66bd993b543f14a36
Islam Muhammed <islammu7ammed.official@gmail.com>
2026-03-18 12:08:33 +0200

    Time: 1 ms (87.16%) | Memory: 54.9 MB (56.41%) - LeetSync

--- 01_Arrays_and_Hashing/1-two-sum/two-sum.js
+++ 01_Arrays_and_Hashing/1-two-sum/two-sum.js
@@ -3,14 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let set =  new Set(nums);
-        for (let i = 0; i<nums.length; i++){
-            if (set.has(target-nums[i])){
-                let j = nums.lastIndexOf(target-nums[i])
-                if (i !== j){
-                    return [i,j];
-                }
-            }
+var twoSum = function (nums, target) {
+    let hash = {};
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (diff in hash) {
+            return [hash[diff], i]
         }
+        hash[nums[i]] = i;
+    }
 };
